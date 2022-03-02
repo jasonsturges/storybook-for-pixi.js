786c6302b7838d38dde60a8a135b653dcb782622
Jason Sturges <jason@jasonsturges.com>
2022-03-02 01:27:52 -0600

    Canvas singleton
    Fixes #6

--- stories/Scene.js
+++ stories/Scene.js
@@ -1,34 +1,46 @@
 import * as PIXI from "pixi.js";
 import { Viewport } from "pixi-viewport";
 
+const canvas = document.createElement("canvas");
+
+const app = new PIXI.Application({
+  view: canvas,
+  width: 300,
+  height: 150,
+  antialias: true,
+  autoDensity: true,
+  backgroundColor: 0x0,
+  resolution: devicePixelRatio,
+});
+
+const viewport = new Viewport({
+  worldWidth: 1000,
+  worldHeight: 1000,
+  screenWidth: 300,
+  screenHeight: 150,
+  divWheel: app.view,
+  interaction: app.renderer.plugins.interaction,
+})
+  .drag()
+  .pinch()
+  .wheel()
+  .decelerate();
+
+app.stage.addChild(viewport);
+app.ticker.start();
+
+export const resize = (width, height) => {
+  app.renderer.resize(width, height);
+  viewport.resize(width, height);
+};
+
 export const createScene = ({ width, height }) => {
-  const canvas = document.createElement("canvas");
+  resize(width, height);
 
-  const app = new PIXI.Application({
-    view: canvas,
-    width: width,
-    height: height,
-    antialias: true,
-    autoDensity: true,
-    backgroundColor: 0x0,
-    resolution: devicePixelRatio,
-  });
-
-  const viewport = new Viewport({
-    worldWidth: 1000,
-    worldHeight: 1000,
-    screenWidth: width,
-    screenHeight: height,
-    divWheel: app.view,
-    interaction: app.renderer.plugins.interaction,
-  })
-    .drag()
-    .pinch()
-    .wheel()
-    .decelerate();
-
-  app.stage.addChild(viewport);
-  app.ticker.start();
+  while (viewport.children.length > 0) {
+    const child = viewport.children.shift();
+    child.destroy(true);
+  }
 
   return {
     canvas,
