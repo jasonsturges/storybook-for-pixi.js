817dd727a26577b80c3640905ca6cf38f02392ff
Jason Sturges <jason@jasonsturges.com>
2022-03-07 23:45:00 -0600

    Star component

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,8 +1,12 @@
-import * as PIXI from "pixi.js";
-import { createScene } from "../../stories/Scene";
-import { parseColor } from "../utils/ColorUtils";
-
-function drawStar(target, x, y, points, innerRadius, outerRadius, angle = 0) {
+export function drawStar(
+  target,
+  x,
+  y,
+  points,
+  innerRadius,
+  outerRadius,
+  angle = 0
+) {
   if (points > 2) {
     let step, halfStep, start, n, dx, dy;
     step = (Math.PI * 2) / points;
@@ -23,35 +27,3 @@ function drawStar(target, x, y, points, innerRadius, outerRadius, angle = 0) {
     target.closePath();
   }
 }
-
-export const createStar = ({
-  width,
-  height,
-  stroke,
-  color,
-  fill,
-  points,
-  innerRadius,
-  outerRadius,
-  angle,
-}) => {
-  const { canvas, app, viewport } = createScene({ width, height });
-
-  const graphics = new PIXI.Graphics();
-  graphics.lineStyle(stroke, parseColor(color));
-  graphics.beginFill(parseColor(fill));
-
-  drawStar(
-    graphics,
-    width / 2,
-    height / 2,
-    points,
-    innerRadius,
-    outerRadius,
-    angle
-  );
-
-  viewport.addChild(graphics);
-
-  return canvas;
-};
