79070c39efbb15fcc5965973a764c3eb5ab14e1e
Jason Sturges <jason@jasonsturges.com>
2022-03-07 23:48:43 -0600

    Logo stories

---
+++ stories/logo/Logo.stories.js
@@ -0,0 +1,19 @@
+import * as PIXI from "pixi.js";
+import { canvas, viewport } from "../Scene";
+
+export default {
+  title: "Example/Logos",
+};
+
+/**
+ * Pixi logo story
+ */
+export const PixiLogo = (args) => {
+  const logo = PIXI.Sprite.from("images/logo.svg");
+  logo.anchor.set(0.5, 0.5);
+  logo.x = viewport.screenWidth / 2;
+  logo.y = viewport.screenHeight / 2;
+  viewport.addChild(logo);
+
+  return canvas;
+};
